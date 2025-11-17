da08c040978c04c793ceb4e3a5fe885492bfbad9
Koti <Koti@LAPTOP-S7AL21H4>
2025-11-17 16:55:55 -0300

    Agregar archivos de proyecto.

---
+++ Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TruequeTextil.Models
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }="";
+
+        [Required(ErrorMessage = "El Apellido es obligatorio")]
+        public string Apellido { get; set; } = "";
+        
+        [Required(ErrorMessage = "El número telefónico es obligatorio")]
+        public string NumeroTelefono { get; set; } = "";
+        
+        [Required(ErrorMessage = "El correo electrónico es obligatorio")]
+        public string CorreoElectronico { get; set; } = "";
+
+        public bool Editando = false;
+    }
+}
+
